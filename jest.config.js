fb19924c4d5c6e47eb6353b1a72684d5e9ba3564
Jussi Kajalin <jkajalin@gmail.com>
2026-01-31 13:02:13 +0200

    testPathIgnorePatterns vipu siirretty jest.config säännöksi

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   testEnvironment: 'node',
   transformIgnorePatterns: [
     'node_modules/(?!(uuid)/)'
-  ]
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/build/",
+    "<rootDir>/node_modules/",
+    "test_helper.js"
+  ],
 };
